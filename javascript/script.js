61720e6812ddf58c42aba44265244ad33ba4ee08
Mustapha Bello <mustaphabello@T-KROSS-MBP.hitronhub.home>
2020-04-29 10:33:53 -0400

    add javascript on contact us and studio pages

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,20 @@
 function confirmMessage(message) {
+    // name variable is used to store user's name
+    
     name = (document.getElementById("visitor_name").value);
+
+    // message variable is used to store user's message
     message = (document.getElementById("message").value);
 
     
-
+    //checking if message field is empty and display error message
     if (message.length() == 0) {
 
         alert("Please enter a message before sending");
-    } else if(name.length() == 0){
+    } 
+
+        //checking if name field is empty and display error message
+    else if(name.length() == 0){
         alert("Please enter your name before sending");
     }
     else{
