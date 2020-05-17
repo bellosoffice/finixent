1c06b742d1fe87f36fcac947a2f3a767732d36b3
Mustapha Bello <mustaphabello@T-KROSS-MBP.hitronhub.home>
2020-05-16 20:13:06 -0400

    final version of website

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,11 @@
-function confirmMessage(message) {
-    // name variable is used to store user's name
-    
-    name = (document.getElementById("visitor_name").value);
 
-    // message variable is used to store user's message
-    message = (document.getElementById("message").value);
+$(".merchandise-btn").click(buyNow);
+function buyNow() {
+  alert("Bought!");
+}
 
-    
-    //checking if message field is empty and display error message
-    if (message.length() == 0) {
+$(".location").click(moveTo);
 
-        alert("Please enter a message before sending");
-    } 
-
-        //checking if name field is empty and display error message
-    else if(name.length() == 0){
-        alert("Please enter your name before sending");
-    }
-    else{
-        alert("Message has been sent");
-    }
-
+function moveTo() {
+  location.replace(ContactUs.html);
 }
